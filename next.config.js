17859dd58301fb5d59dab71acbec7c3ff92c11c1
handy-o <dyhan9420@gamil.com>
2022-07-27 21:26:30 +0900

    에러나서 되돌립니다. #2.1Fetching Data

--- next.config.js
+++ next.config.js
@@ -1,39 +1,20 @@
-/** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
-
-// module.exports = nextConfig
-
 const API_KEY = process.env.API_KEY;
+
 module.exports = {
-	reactStricMode: true,
+	reactStrictMode: true,
 	async redirects() {
 		return [
-			// array 를 return
 			{
-				// 객체로 된 array
-				source: '/contact',
-				destination: '/form',
-				permanent: false,
-			},
-			{
-				source: '/old-blog/:path', //뒤에 붙은 :paht(ex_postID)는 동일
-				destination: '/new-blog/:path', //catch 된 것
-				permanent: false,
-			},
-			{
-				source: '/old-blog/:path*', //뒤에 어떤 경로가 붙어도
-				destination: '/new-blog/:path*', //동일하게 catch 된 것
+				source: '/old-blog/:path*',
+				destination: '/new-sexy-blog/:path*',
 				permanent: false,
 			},
 		];
 	},
-
 	async rewrites() {
 		return [
 			{
-				source: '/api/movies', //<<fecth 주소에 이 URL을 넣음
+				source: '/api/movies',
 				destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
 			},
 			{
--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,36 @@
-import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import Seo from './components/Seo';
 
-// const API_KEY = "10923b261ba94d897ac6b81148314a3f"; // next.config.js로 이동
+const API_KEY = '10923b261ba94d897ac6b81148314a3f';
 
-export default function Home({ results }) {
-	const router = useRouter();
-	const onClick = (id) => {
-		router.push(`/movies/${id}`);
-	};
-	/* const [Movies, setMovies] = useState();
+export default function Home() {
+	const [movies, setMovies] = useState();
 	useEffect(() => {
 		(async () => {
-			//const reponse = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)    })
-			//const json = await Response.json();
 			const { results } = await (
 				await fetch(
-					// `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}` // next.config.js로 이동
-					`/api/movies`
+					`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
 				)
 			).json();
 			setMovies(results);
 		})();
-	}, []); */
+	}, []);
 	return (
 		<div className='container'>
 			<Seo title='Home' />
-			{/* {!Movies && <h4>Loading...</h4>} {/*mount되고 기다렸다가*/}
-			{/*화면 출력*/}
-			{results?.map((movie) => (
-				<div onClick={() => onClick(movie.id)} className='movie'>
-					<img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-					<h4>
-						<Link href={`/movies/${movie.id}`} key={movie.id}>
-							<a>{movie.original_title}</a>
-						</Link>
-					</h4>
+			{!movies && <h4>Loading...</h4>}
+			{movies?.map((movie) => (
+				<div className='movie' key={movie.id}>
+					<img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+					<h4>{movie.original_title}</h4>
 				</div>
 			))}
-			{/* 스타일 */}
 			<style jsx>{`
 				.container {
 					display: grid;
 					grid-template-columns: 1fr 1fr;
 					padding: 20px;
 					gap: 20px;
-				}
-				.movie {
-					cursor: pointer;
 				}
 				.movie img {
 					max-width: 100%;
@@ -65,15 +48,4 @@ export default function Home({ results }) {
 			`}</style>
 		</div>
 	);
-}
-
-export async function getServerSideProps() {
-	const { results } = await (
-		await fetch(`http://localhost:3000/api/movies`)
-	).json();
-	return {
-		props: {
-			results,
-		},
-	};
 }
