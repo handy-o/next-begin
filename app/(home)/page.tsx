1ab5a97e32acd70c51232bd469415066005f372b
handy-o <dyhan9420@gmail.com>
2024-12-09 23:24:46 +0900

    1일차_4_fetch Data

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,20 +1,19 @@
+import Link from "next/link";
+
 export const metadata = {
     title: 'Home'
 }
-const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
     // await new Promise((res) => setTimeout(res, 5000))
-    const resp = await fetch(URL); // NextJS에서 fetch된 url을 캐싱 
+    const resp = await fetch(API_URL); // NextJS에서 fetch된 url을 캐싱 
     const json = await resp.json();
     return json;
 }
 export default async function HomePage() {
     const movies = await getMovies();
     return (
-        <div>
-            <h1>Hello</h1>
-            {JSON.stringify(movies)}
-        </div>
+        <div> {movies.map(movie => <li><Link href={`/movies/${movie.id}`}>{movie.title}</Link></li>)}</div>
     )
 }
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,8 +1,34 @@
-export default function MovieDetail({
+import { Suspense } from "react";
+// import { API_URL } from "../../../(home)/page";
+import MovieInfo from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-video";
+
+// async function getMovie(id:string) {
+//     const response = await fetch(`${API_URL}/${id}`);
+//     return response.json();
+// }
+
+// async function getVideos(id: string) {
+//     const response = await fetch(`${API_URL}/${id}/videos`);
+//     return response.json();
+// }
+
+export default async function MovieDetail({
     params: {id},
     }: {
     params: {id: string};
     }) {
     // console.log('props', props)
-    return <h1>Movie {id} </h1>
+    // const movie = await getMovie(id); // 순차
+    // const videos = await getVideos(id); // 실행
+    // const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]); // 병렬 실행
+    // return <h1>{movie.title} </h1>
+    return <div>
+        <Suspense fallback={<h1>Loading movie Info</h1>}>
+            <MovieInfo id={id}/>
+        </Suspense>
+        <Suspense fallback={<h1>Loading movie videos</h1>}>
+            <MovieVideos id={id}/>
+        </Suspense>
+    </div>
 }
