5d5373d8bb8e72e2586c145631988005d2dbee5f
handy-o <dyhan9420@gmail.com>
2024-12-20 14:08:56 +0900

    7~8일차 인풋 초기화되던 이유 14,15버전

--- components/form-input.tsx
+++ components/form-input.tsx
@@ -9,12 +9,13 @@ interface formInputProps {
 export default function FormInput({name, type, placeholder, required, errors}: formInputProps) {
     return (
         <div>
+            {/* 14 버전 : refresh 안되고 input 에 값 남아있음
+                15 버전 : refresh 되면서 input 다 날아감 */}
             <input 
                 name={name}
                 type={type}
                 placeholder={placeholder}
                 required={required}
-                defaultValue=""
                 className="w-full h-8 border border-1 border-solid border-gray-300 rounded-2xl focus:outline focus:outline-1 focus:outline-offset-2 focus:outline-gray-400 p-1 pl-10"/>
 
             {errors.map((error, index) => (
