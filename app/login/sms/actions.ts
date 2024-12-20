9074c0a6285731fcac66c1c65e0048813d405618
handy-o <dyhan9420@gmail.com>
2024-12-21 00:26:45 +0900

    10일차 4_ interactive form (prevState사용)

--- app/login/sms/actions.ts
+++ app/login/sms/actions.ts
@@ -10,12 +10,46 @@
 // 각각 검사할거라 object로 묶지 않아도 됨
 import {z} from "zod"
 import validator from "validator"  // typescript에서 type이 지정되지 않은 라이브러리 사용 시 누군가 다 적어놓은 npm i --save-dev @types/validator 를 추가로 실행
+import { redirect } from "next/navigation";
 
-const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
-const tokenSchema = z.coerce.number().min(100000).max(999999)
+const phoneSchema = z.string()
+                    .trim()
+                    .refine(phone => validator.isMobilePhone(phone, "ko-KR"), "Wrong phone format"); // 유저가 입력한 전화번호와 locale 함께 호출
+const tokenSchema = z.coerce.number().min(100000).max(999999);
 // coerce(강제) => 유저가 입력한  string을 number로 변환 시도 (문자 쓰면 에러남)
 
-export async function smsVerification(prevState: any, formData: FormData) {
-    tokenSchema.parse(formData.get("token"))
+interface ActionState {
+    token: boolean,
+}
+
+export async function smsVerification(prevState: ActionState, formData: FormData) {
+    //tokenSchema.parse(formData.get("token"))
+
+    const phone = formData.get("phone");
+    const token = formData.get("token");
+
+    if (!prevState.token) {
+    const result = phoneSchema.safeParse(phone);
+    if (!result.success) {
+      return {
+        token: false,
+        error: result.error.flatten(),
+      };
+    } else {
+      return {
+        token: true,
+      };
+    }
+  } else {
+    const result = tokenSchema.safeParse(token);
+    if (!result.success) {
+      return {
+        token: true,
+        error: result.error.flatten(),
+      };
+    } else {
+      redirect("/");
+    }
+  }
     
 }
--- app/login/sms/page.tsx
+++ app/login/sms/page.tsx
@@ -1,11 +1,19 @@
+"use client";
+
 import Input from "../../components/input";
 import Button from "../../components/button";
 import SocialLogin from "@/components/social-login";
 import { useActionState } from "react";
 import { smsVerification } from "./actions";
 
+const initialState = {
+  token: false,
+  error: undefined,
+}
+
 export default function SMSLogin() {
-  const [state, dispatch] = useActionState(smsVerification, null);
+  // const [state, dispatch] = useActionState(smsVerification, null); // null == initial state == prevState
+  const [state, dispatch] = useActionState(smsVerification, initialState); // null을 수정
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
@@ -19,17 +27,21 @@ export default function SMSLogin() {
           type="text" 
           placeholder="Phone number"
           required
+          errors={state.error?.formErrors}
           />
-        <Input 
-          name="token"
-          type="number" 
-          placeholder="Verification code"
-          required
-          min={100000}
-          max={999999}
-          />
+          {
+            state.token ? (<Input 
+              name="token"
+              type="number" 
+              placeholder="Verification code"
+              required
+              min={100000}
+              max={999999}
+              />) : null
+          }
         
-        <Button text={'Verify'} />
+        
+        <Button text={state?.token ? 'Verify' : "Send Verification SMS"} />
       </form>
       <SocialLogin/>
     </div>
