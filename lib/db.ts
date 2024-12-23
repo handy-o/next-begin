f01fd55a127b84f87cd86312ba21415dd2161a3a
handy-o <dyhan9420@gmail.com>
2024-12-23 19:34:58 +0900

    11일차 1_prisma #7

---
+++ lib/db.ts
@@ -0,0 +1,51 @@
+import { PrismaClient } from "@prisma/client";
+
+const db = new PrismaClient();
+
+async function test2() {
+    // const token = await db.sMSToken.create({
+    //     data: {
+    //         token: "123123",
+    //         user: {
+    //             connect: {
+    //                 id: 1
+    //             }
+    //         }
+    //     }
+    // })
+    const token = await db.sMSToken.findUnique({
+        where: {
+           id: 1
+        },
+        include : {
+          user: true  
+        }
+    })
+    console.log('findUnique', token)
+}
+test2()
+
+
+async function test(){
+    // 생성
+    const user = await db.user.create({
+        data: { 
+            username: "lala",
+            phone: "123123123",
+        }
+    })
+    console.log(user)
+
+    // 찾기
+    const users = await db.user.findMany({
+        where: { 
+            username: {
+                contains : "la"
+            },
+        }
+    })
+    console.log(user)
+}
+// test();
+
+export default db;
