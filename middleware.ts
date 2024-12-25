cefd91f254b34ee97efe2d5e8d6ce0aec341ab6a
handy-o <dyhan9420@gmail.com>
2024-12-25 22:00:37 +0900

    12일차 7_ Matcher,  Edge Runtime

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,27 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import getSession from "./lib/session";
+import db from "./lib/db";
 
-export async function middleware(request: NextRequest) {
-    // url 읽기
-    // console.log('Hi im middleware')
-    // console.log('request.url', request.url)
-    // console.log(request.nextUrl.pathname)
-
-    if(request.nextUrl.pathname === "/profile") {
-        // return Response.json({ // fetch API의 Response
-        //     error: "you are not allowed here!" 
-        // }) 
-
-        // return Response.redirect("/") // 으로만 하면 절대경로로 생각하고 에러가 발생
+export async function middleware(request: NextRequest) { // 이름은 꼭 middleware
+    const pathname = request.nextUrl.pathname;
+    if(pathname === "/") {
+        // set cookie
+        const response = NextResponse.next(); // request 가로채서 
+        response.cookies.set("middleware-cookie", "hello!") // 정보를 추가하여 수정한 뒤에
+        return response; // 그 request를 user에게 제공
+    }
+    if(pathname  === "/profile") {
         return Response.redirect(new URL("/", request.url))
     }
-    
-    // 쿠키 읽기 
-    // console.log(request.cookies.getAll());
-    // const session = await getSession();
-    // console.log(session)
+
+    // await db.user.findMany({}); // Edge runtime 이슈로 실행되지 않고 서버 에러 발생
+
+}
+
+
+// 이름은 꼭 config 
+export const config = {
+    matcher: ["/", "/profile", "/create-account", "/user/:path*"]
+    // matcher: ["/((?!api|_next/static|_next/image|favicon.ico|logo.svg).*)"]
 }
